b00f36ab972892bb99207df0cd0639692ea409c5
Fedor <fedor.tkachenko@eventinc.de>
2020-08-27 23:56:45 +0200

    refa: refactor main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import MainPage from 'pages/MainPage';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-      </header>
-    </div>
+    <MainPage />
   );
 }
 
