72eaed9d682274b9dfb5c9dea869aeaa03db6d6e
Fedor <fedor.tkachenko@eventinc.de>
2020-09-11 20:22:21 +0200

    feat: deal with routing

--- src/Routes/routesList.ts
+++ src/Routes/routesList.ts
@@ -9,7 +9,7 @@ const routes: Array<RouteProps> = [
   { path: '/', component: MainPage },
   { path: '/list', component: Options },
   { path: '/list/create', component: CreateList },
-  { path: '/list/create/:id', component: Options }, // add component instead of Options here to handle saved list
+  { path: '/list/create/#:id', component: CreateList },
 ]
 
 export default routes
