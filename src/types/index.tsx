1db345364d490893cf5b5d62db68bc5863359a91
Fedor <fedor.tkachenko@eventinc.de>
2020-09-11 21:33:25 +0200

    feat: add generic page props

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -3,5 +3,14 @@ export interface Task {
   checked: boolean,
 }
 
+export interface PageProps {
+  history: {
+    push: (newPath: string) => void,
+  },
+  location: {
+    hash: string,
+  }
+}
+
 export const DEFAULT_TASK: Task = { text: '', checked: false }
 export const DEFAULT_TASK_TEXT: string = 'Nothing yet...'
