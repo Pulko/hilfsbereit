26b76e53b242790598f46ec470277dec0e12f683
Fedor <fedor.tkachenko@eventinc.de>
2020-09-12 14:39:10 +0200

    Updates

---
+++ precache-manifest.1dacce39d2f1f685df965eb512267e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee65d8deba4a8d360f966e51e4f023e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e3b0618051d8d1ecc4d",
+    "url": "/static/css/main.ad5870b7.chunk.css"
+  },
+  {
+    "revision": "6e0f114b5c109478f9f5",
+    "url": "/static/js/2.5efe0d1e.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.5efe0d1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3b0618051d8d1ecc4d",
+    "url": "/static/js/main.ff929379.chunk.js"
+  },
+  {
+    "revision": "8b49d2e0d16bd5c9d533",
+    "url": "/static/js/runtime-main.5de75b46.js"
+  }
+]);
